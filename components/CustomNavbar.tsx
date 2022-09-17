779cb06dce1a06fb34094a528fd7e02f65986d86
Oscar Figueroa <fisocodes@gmail.com>
2022-09-16 18:28:34 -0700

    Custom navbar changed

--- components/CustomNavbar.tsx
+++ components/CustomNavbar.tsx
@@ -17,6 +17,7 @@ import { signOut } from 'next-auth/react'
 import { useSession } from 'next-auth/react'
 
 //Mantine
+import { useMantineTheme } from '@mantine/core'
 import { Navbar } from '@mantine/core'
 import { NavLink } from '@mantine/core'
 import { Avatar } from '@mantine/core'
@@ -24,9 +25,15 @@ import { Title } from '@mantine/core'
 import { Center } from '@mantine/core'
 import { Stack } from '@mantine/core'
 
+//Icons
+import { IconUsers } from '@tabler/icons'
+import { IconLogout } from '@tabler/icons'
+import { IconLayoutDashboard } from '@tabler/icons'
+
 
 export default function CustomNavbar(){
 
+    const theme = useMantineTheme()
     const router = useRouter()
     const {data: session, status} = useSession()
     const [user, setUser] = useState(null)
@@ -42,16 +49,17 @@ export default function CustomNavbar(){
             <Stack spacing='xs'>
                 <Center>
                     <Stack align='center'>
-                        <Avatar size={125} radius='xs'>
+                        <Avatar size={125} radius={100}>
                             {`${user?.name[0].toUpperCase()}${user?.lastname[0].toUpperCase()}`}
                         </Avatar>
                         <Title order={4}>{`${user?.name} ${user?.lastname}`}</Title>
                     </Stack>
                 </Center>
-                <NavLink onClick={() => router.push('/users')} label='Usuarios'/>
+                <NavLink onClick={() => router.push('/')} active={router.pathname === '/'} variant='filled' label='Dasboard' icon={<IconLayoutDashboard/>}/>
+                <NavLink onClick={() => router.push('/users')} active={router.pathname === '/users'} variant='filled' label='Usuarios' icon={<IconUsers/>}/>
             </Stack>
             <Stack spacing={0}>
-                <NavLink onClick={() => signOut()} label='Cerrar sesión' color='red' active variant='subtle'/>
+                <NavLink onClick={() => signOut()} label='Cerrar sesión' color='red' active variant='light' icon={<IconLogout/>}/>
             </Stack>
         </Stack>
     </Navbar>
